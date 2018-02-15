cb405ef026a8f89cf0c366fcee70bb7fcaf592b8
Petr Vranik <petr.vranik@lmc.eu>
2018-02-15 17:19:50 +0100

    sdk16 support

--- app/src/main/java/net/suteren/halauncher/activity/MainActivity.java
+++ app/src/main/java/net/suteren/halauncher/activity/MainActivity.java
@@ -95,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("SetJavaScriptEnabled")
     private void setupWebView() {
+
         webView = (WebView) findViewById(R.id.fullscreen_content);
         webView.setBackgroundColor(Color.BLACK);
         //webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
@@ -185,15 +186,29 @@ public class MainActivity extends AppCompatActivity {
             actionBar.hide();
         }
 
+        if (Build.VERSION.SDK_INT >= 19) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        } else  {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+            int flags = View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+            findViewById(android.R.id.content).setSystemUiVisibility(flags);
+            webView.setSystemUiVisibility(flags);
+        }
+
+
         // Schedule a runnable to remove the status and navigation bar after a delay
-        webView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 
     // Called whenever we need to wake up the device
