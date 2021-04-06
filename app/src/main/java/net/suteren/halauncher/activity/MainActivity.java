407ad77f31a839ab1adf7a6e94312b024dd762d8
pvranik <petr.vranik@mgm-tp.com>
2021-04-06 12:22:50 +0200

    android upgrade

--- app/src/main/java/net/suteren/halauncher/activity/MainActivity.java
+++ app/src/main/java/net/suteren/halauncher/activity/MainActivity.java
@@ -1,7 +1,8 @@
 package net.suteren.halauncher.activity;
 
+import java.util.Locale;
+
 import android.annotation.SuppressLint;
-import android.app.Application;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
@@ -10,9 +11,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.provider.Settings;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -23,12 +21,12 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.GridView;
-
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import net.suteren.halauncher.DayTimeReceiver;
 import net.suteren.halauncher.R;
 import net.suteren.halauncher.adapter.AppAdapter;
-
-import java.util.Locale;
 
 import static android.net.wifi.WifiManager.WIFI_MODE_FULL;
 import static android.os.PowerManager.ACQUIRE_CAUSES_WAKEUP;
@@ -49,206 +47,205 @@ import static android.view.WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON;
  */
 public class MainActivity extends AppCompatActivity {
 
-    public static final String JSCONSOLE = "jsconsole";
-    public static final String JALERT = "jalert";
-    public static final String RELOAD_ACTION = "reload";
-    public static final String WAKEUP_ACTION = "wakeup";
-    private WebView webView;
-    private PowerManager.WakeLock partial;
-    private WifiManager.WifiLock wl;
+	public static final String JSCONSOLE = "jsconsole";
+	public static final String JALERT = "jalert";
+	public static final String RELOAD_ACTION = "reload";
+	public static final String WAKEUP_ACTION = "wakeup";
+	private WebView webView;
+	private PowerManager.WakeLock partial;
+	private WifiManager.WifiLock wl;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        partial = acquireWakelock(PARTIAL_WAKE_LOCK);
-        wl = acquireWifiLock();
+		partial = acquireWakelock(PARTIAL_WAKE_LOCK);
+		wl = acquireWifiLock();
 
-        final GridView appsView = (GridView) findViewById(R.id.menu);
-        AppAdapter adapter = new AppAdapter(this);
-        appsView.setAdapter(adapter);
+		final GridView appsView = (GridView) findViewById(R.id.menu);
+		AppAdapter adapter = new AppAdapter(this);
+		appsView.setAdapter(adapter);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                super.onDrawerClosed(drawerView);
-                ((AppAdapter) appsView.getAdapter()).updateActs();
-            }
-        });
-        //setDrawerLeftEdgeSize(this, drawer, 1f);
-        getWindow().addFlags(FLAG_DISMISS_KEYGUARD | FLAG_KEEP_SCREEN_ON | FLAG_TURN_SCREEN_ON | FLAG_SHOW_WHEN_LOCKED);
-        setupWebView();
-        reloadWeb();
-        registerReceiver(new DayTimeReceiver(), new IntentFilter(Intent.ACTION_TIME_TICK));
-    }
+		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+		drawer.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+			@Override
+			public void onDrawerClosed(View drawerView) {
+				super.onDrawerClosed(drawerView);
+				((AppAdapter) appsView.getAdapter()).updateActs();
+			}
+		});
+		//setDrawerLeftEdgeSize(this, drawer, 1f);
+		getWindow().addFlags(FLAG_DISMISS_KEYGUARD | FLAG_KEEP_SCREEN_ON | FLAG_TURN_SCREEN_ON | FLAG_SHOW_WHEN_LOCKED);
+		setupWebView();
+		reloadWeb();
+		registerReceiver(new DayTimeReceiver(), new IntentFilter(Intent.ACTION_TIME_TICK));
+	}
 
-    private WifiManager.WifiLock acquireWifiLock() {
-        Settings.System.putInt(getContentResolver(), WIFI_SLEEP_POLICY, WIFI_SLEEP_POLICY_NEVER);
-        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
-        assert wifiManager != null;
-        WifiManager.WifiLock fullWifi = wifiManager.createWifiLock(WIFI_MODE_FULL, "fullWifi");
-        fullWifi.acquire();
-        return fullWifi;
-    }
+	private WifiManager.WifiLock acquireWifiLock() {
+		Settings.System.putInt(getContentResolver(), WIFI_SLEEP_POLICY, WIFI_SLEEP_POLICY_NEVER);
+		WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+		assert wifiManager != null;
+		WifiManager.WifiLock fullWifi = wifiManager.createWifiLock(WIFI_MODE_FULL, "fullWifi");
+		fullWifi.acquire();
+		return fullWifi;
+	}
 
-    @Override
-    protected void onPostResume() {
-        super.onPostResume();
-    }
+	@Override
+	protected void onPostResume() {
+		super.onPostResume();
+	}
 
-    @SuppressLint("SetJavaScriptEnabled")
-    private void setupWebView() {
+	@SuppressLint("SetJavaScriptEnabled")
+	private void setupWebView() {
 
-        webView = (WebView) findViewById(R.id.fullscreen_content);
-        webView.setBackgroundColor(Color.BLACK);
-        //webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
-        webView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return (event.getAction() == MotionEvent.ACTION_MOVE);
-            }
-        });
-        webView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                return true;
-            }
-        });
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setAppCacheEnabled(true);
-        settings.setBuiltInZoomControls(false);
-        settings.setDatabaseEnabled(true);
-        settings.setDisplayZoomControls(false);
-        settings.setDomStorageEnabled(true);
-        //settings.setLayoutAlgorithm(SINGLE_COLUMN);
-        webView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
-                String msg = String.format(Locale.getDefault(), "js (%s:%d): %s", consoleMessage.sourceId(), consoleMessage.lineNumber(), consoleMessage.message());
-                switch (consoleMessage.messageLevel()) {
-                    case DEBUG:
-                        Log.d(JSCONSOLE, msg);
-                        break;
-                    case ERROR:
-                        Log.e(JSCONSOLE, msg);
-                        break;
-                    case TIP:
-                        Log.v(JSCONSOLE, msg);
-                        break;
-                    case WARNING:
-                        Log.w(JSCONSOLE, msg);
-                        break;
-                    case LOG:
-                        Log.i(JSCONSOLE, msg);
-                        break;
-                }
-                return true;
-            }
+		webView = (WebView) findViewById(R.id.fullscreen_content);
+		webView.setBackgroundColor(Color.BLACK);
+		//webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+		webView.setOnTouchListener(new View.OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				return (event.getAction() == MotionEvent.ACTION_MOVE);
+			}
+		});
+		webView.setOnLongClickListener(new View.OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View v) {
+				return true;
+			}
+		});
+		WebSettings settings = webView.getSettings();
+		settings.setJavaScriptEnabled(true);
+		settings.setAppCacheEnabled(true);
+		settings.setBuiltInZoomControls(false);
+		settings.setDatabaseEnabled(true);
+		settings.setDisplayZoomControls(false);
+		settings.setDomStorageEnabled(true);
+		//settings.setLayoutAlgorithm(SINGLE_COLUMN);
+		webView.setWebChromeClient(new WebChromeClient() {
+			@Override
+			public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+				String msg = String.format(Locale.getDefault(), "js (%s:%d): %s", consoleMessage.sourceId(), consoleMessage.lineNumber(), consoleMessage.message());
+				switch (consoleMessage.messageLevel()) {
+				case DEBUG:
+					Log.d(JSCONSOLE, msg);
+					break;
+				case ERROR:
+					Log.e(JSCONSOLE, msg);
+					break;
+				case TIP:
+					Log.v(JSCONSOLE, msg);
+					break;
+				case WARNING:
+					Log.w(JSCONSOLE, msg);
+					break;
+				case LOG:
+					Log.i(JSCONSOLE, msg);
+					break;
+				}
+				return true;
+			}
 
-            @Override
-            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
-                result.cancel();
-                Log.w(JALERT, url + "|" + message);
-                return true;
-            }
-        });
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                    webView.evaluateJavascript("$('body').css('-webkit-tap-highlight-color', 'rgba(0, 0, 0, 0)')", null);
-                }
-                super.onPageFinished(view, url);
-                Log.i(JSCONSOLE, "Page finished");
-            }
+			@Override
+			public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+				result.cancel();
+				Log.w(JALERT, url + "|" + message);
+				return true;
+			}
+		});
+		webView.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+					webView.evaluateJavascript("$('body').css('-webkit-tap-highlight-color', 'rgba(0, 0, 0, 0)')", null);
+				}
+				super.onPageFinished(view, url);
+				Log.i(JSCONSOLE, "Page finished");
+			}
 
-            @Override
-            public void onLoadResource(WebView view, String url) {
-                super.onLoadResource(view, url);
-                Log.i(JSCONSOLE, "Resource loaded: " + url);
-            }
-        });
-    }
+			@Override
+			public void onLoadResource(WebView view, String url) {
+				super.onLoadResource(view, url);
+				Log.i(JSCONSOLE, "Resource loaded: " + url);
+			}
+		});
+	}
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        delayedHide();
-        webView.resumeTimers();
-    }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		delayedHide();
+		webView.resumeTimers();
+	}
 
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
+	/**
+	 * Schedules a call to hide() in [delay] milliseconds, canceling any
+	 * previously scheduled calls.
+	 */
+	private void delayedHide() {
+		// Hide UI first
+		ActionBar actionBar = getSupportActionBar();
+		if (actionBar != null) {
+			actionBar.hide();
+		}
 
-        if (Build.VERSION.SDK_INT >= 19) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        } else  {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
-            int flags = View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-            findViewById(android.R.id.content).setSystemUiVisibility(flags);
-            webView.setSystemUiVisibility(flags);
-        }
+		if (Build.VERSION.SDK_INT >= 19) {
+			getWindow().getDecorView().setSystemUiVisibility(
+				View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+					| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+					| View.SYSTEM_UI_FLAG_FULLSCREEN
+					| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+					| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+					| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+		} else {
+			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+			int flags = View.SYSTEM_UI_FLAG_LOW_PROFILE
+				| View.SYSTEM_UI_FLAG_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+				| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+				| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+			findViewById(android.R.id.content).setSystemUiVisibility(flags);
+			webView.setSystemUiVisibility(flags);
+		}
 
+		// Schedule a runnable to remove the status and navigation bar after a delay
+	}
 
-        // Schedule a runnable to remove the status and navigation bar after a delay
-    }
+	// Called whenever we need to wake up the device
+	public void wakeDevice() {
+		getWindow().addFlags(FLAG_DISMISS_KEYGUARD | FLAG_KEEP_SCREEN_ON | FLAG_TURN_SCREEN_ON | FLAG_SHOW_WHEN_LOCKED);
+		acquireWakelock(SCREEN_BRIGHT_WAKE_LOCK | FULL_WAKE_LOCK | ACQUIRE_CAUSES_WAKEUP);
+	}
 
-    // Called whenever we need to wake up the device
-    public void wakeDevice() {
-        getWindow().addFlags(FLAG_DISMISS_KEYGUARD | FLAG_KEEP_SCREEN_ON | FLAG_TURN_SCREEN_ON | FLAG_SHOW_WHEN_LOCKED);
-        acquireWakelock(SCREEN_BRIGHT_WAKE_LOCK | FULL_WAKE_LOCK | ACQUIRE_CAUSES_WAKEUP);
-    }
+	@SuppressLint("WakelockTimeout")
+	private PowerManager.WakeLock acquireWakelock(int levelAndFlags) {
+		PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+		assert powerManager != null;
+		PowerManager.WakeLock wakeLock = powerManager
+			.newWakeLock(levelAndFlags, getClass().getName());
+		wakeLock.acquire();
+		return wakeLock;
+	}
 
-    @SuppressLint("WakelockTimeout")
-    private PowerManager.WakeLock acquireWakelock(int levelAndFlags) {
-        PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
-        assert powerManager != null;
-        PowerManager.WakeLock wakeLock = powerManager
-                .newWakeLock(levelAndFlags, getClass().getName());
-        wakeLock.acquire();
-        return wakeLock;
-    }
+	@Override
+	protected void onNewIntent(Intent intent) {
+		super.onNewIntent(intent);
+		Log.d("main", "Action: " + intent.getAction());
+		Log.d("main", "Data: " + intent.getData());
+		if (WAKEUP_ACTION.equals(intent.getAction())) {
+			wakeDevice();
+		} else if (RELOAD_ACTION.equals(intent.getAction())) {
+			reloadWeb();
+		}
+	}
 
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        Log.d("main", "Action: " + intent.getAction());
-        Log.d("main", "Data: " + intent.getData());
-        if (WAKEUP_ACTION.equals(intent.getAction())) {
-            wakeDevice();
-        } else if (RELOAD_ACTION.equals(intent.getAction())) {
-            reloadWeb();
-        }
-    }
-
-    private void reloadWeb() {
-        webView.clearCache(true);
-        webView.clearFormData();
-        webView.clearHistory();
-        webView.clearMatches();
-        webView.loadUrl(getDefaultSharedPreferences(this).getString("url", getResources().getString(R.string.pref_default_url)));
-    }
+	private void reloadWeb() {
+		webView.clearCache(true);
+		webView.clearFormData();
+		webView.clearHistory();
+		webView.clearMatches();
+		webView.loadUrl(getDefaultSharedPreferences(this).getString("url", getResources().getString(R.string.pref_default_url)));
+	}
 }
--- app/src/main/java/net/suteren/halauncher/adapter/AppAdapter.java
+++ app/src/main/java/net/suteren/halauncher/adapter/AppAdapter.java
@@ -1,5 +1,11 @@
 package net.suteren.halauncher.adapter;
 
+import java.text.Collator;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -8,22 +14,15 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Build;
-import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import androidx.annotation.NonNull;
 import net.suteren.halauncher.R;
 import net.suteren.halauncher.activity.MainActivity;
 import net.suteren.halauncher.activity.SettingsActivity;
-
-import java.text.Collator;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
 
 import static android.content.Intent.ACTION_MAIN;
 import static android.content.Intent.CATEGORY_LAUNCHER;
@@ -34,115 +33,120 @@ import static android.preference.PreferenceManager.getDefaultSharedPreferences;
  */
 public class AppAdapter extends BaseAdapter {
 
-    private final SharedPreferences.OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
-        @Override
-        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-            updateActs();
-        }
-    };
-    protected Context context;
-    protected List<ResolveInfo> acts;
-    protected final PackageManager packageManager;
-    private final Collator collator = Collator.getInstance();
-    protected final SharedPreferences prefs;
-    public static final String APPS_KEY = "apps";
+	private final SharedPreferences.OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+		@Override
+		public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+			updateActs();
+		}
+	};
+	protected Context context;
+	protected List<ResolveInfo> acts;
+	protected final PackageManager packageManager;
+	private final Collator collator = Collator.getInstance();
+	protected final SharedPreferences prefs;
+	public static final String APPS_KEY = "apps";
 
-    public AppAdapter(Context mainActivity) {
-        this.context = mainActivity;
-        packageManager = mainActivity.getApplicationContext().getPackageManager();
-        prefs = getDefaultSharedPreferences(context);
-        prefs.registerOnSharedPreferenceChangeListener(listener);
-        updateActs();
-    }
+	public AppAdapter(Context mainActivity) {
+		this.context = mainActivity;
+		packageManager = mainActivity.getApplicationContext().getPackageManager();
+		prefs = getDefaultSharedPreferences(context);
+		prefs.registerOnSharedPreferenceChangeListener(listener);
+		updateActs();
+	}
 
-    public void updateActs() {
-        acts = packageManager.queryIntentActivities(createLauncherIntent(), 0);
-        Iterator<ResolveInfo> it = acts.iterator();
-        while (it.hasNext()) {
-            if (!filterApps(it.next()))
-                it.remove();
-        }
-        Collections.sort(acts, new Comparator<ResolveInfo>() {
-            @Override
-            public int compare(ResolveInfo o1, ResolveInfo o2) {
-                if (o1 == null && o2 == null) return 0;
-                if (o1 == null) return -1;
-                if (o2 == null) return 1;
-                if (context.getPackageName().equals(o1.activityInfo.packageName)) return 1;
-                if (context.getPackageName().equals(o2.activityInfo.packageName)) return -1;
-                if (isSystem(o1) && isSystem(o2)) {
-                    return collator.compare(o1.loadLabel(packageManager), o2.loadLabel(packageManager));
-                } else {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                        return Boolean.compare(isSystem(o2), isSystem(o1));
-                    } else {
-                        return 0;
-                    }
-                }
-            }
-        });
-        notifyDataSetInvalidated();
-        notifyDataSetChanged();
-    }
+	public void updateActs() {
+		acts = packageManager.queryIntentActivities(createLauncherIntent(), 0);
+		Iterator<ResolveInfo> it = acts.iterator();
+		while (it.hasNext()) {
+			if (!filterApps(it.next()))
+				it.remove();
+		}
+		Collections.sort(acts, new Comparator<ResolveInfo>() {
+			@Override
+			public int compare(ResolveInfo o1, ResolveInfo o2) {
+				if (o1 == null && o2 == null)
+					return 0;
+				if (o1 == null)
+					return -1;
+				if (o2 == null)
+					return 1;
+				if (context.getPackageName().equals(o1.activityInfo.packageName))
+					return 1;
+				if (context.getPackageName().equals(o2.activityInfo.packageName))
+					return -1;
+				if (isSystem(o1) && isSystem(o2)) {
+					return collator.compare(o1.loadLabel(packageManager), o2.loadLabel(packageManager));
+				} else {
+					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+						return Boolean.compare(isSystem(o2), isSystem(o1));
+					} else {
+						return 0;
+					}
+				}
+			}
+		});
+		notifyDataSetInvalidated();
+		notifyDataSetChanged();
+	}
 
-    protected boolean filterApps(ResolveInfo n) {
-        return prefs.getStringSet("apps", Collections.EMPTY_SET).contains(new ComponentName(n.activityInfo.packageName, n.activityInfo.name).toShortString()) || (context.getPackageName().equals(n.activityInfo.packageName) && SettingsActivity.class.getName().equals(n.activityInfo.name));
-    }
+	protected boolean filterApps(ResolveInfo n) {
+		return prefs.getStringSet("apps", Collections.EMPTY_SET).contains(new ComponentName(n.activityInfo.packageName, n.activityInfo.name).toShortString()) || (context.getPackageName().equals(n.activityInfo.packageName) && SettingsActivity.class.getName().equals(n.activityInfo.name));
+	}
 
-    private boolean isSystem(ResolveInfo o1) {
-        return 0 == (o1.activityInfo.flags & ApplicationInfo.FLAG_SYSTEM);
-    }
+	private boolean isSystem(ResolveInfo o1) {
+		return 0 == (o1.activityInfo.flags & ApplicationInfo.FLAG_SYSTEM);
+	}
 
-    @Override
-    public int getCount() {
-        return acts.size();
-    }
+	@Override
+	public int getCount() {
+		return acts.size();
+	}
 
-    @Override
-    public ResolveInfo getItem(int position) {
-        return acts.get(position);
-    }
+	@Override
+	public ResolveInfo getItem(int position) {
+		return acts.get(position);
+	}
 
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
 
-    @Override
-    public boolean isEnabled(int position) {
-        return !(MainActivity.class.getPackage().equals(getItem(position).activityInfo.packageName) && MainActivity.class.getName().equals(getItem(position).activityInfo.name));
-    }
+	@Override
+	public boolean isEnabled(int position) {
+		return !(MainActivity.class.getPackage().equals(getItem(position).activityInfo.packageName) && MainActivity.class.getName().equals(getItem(position).activityInfo.name));
+	}
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = View.inflate(context.getApplicationContext(), R.layout.app_info, null);
-        }
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if (convertView == null) {
+			convertView = View.inflate(context.getApplicationContext(), R.layout.app_info, null);
+		}
 
-        final ResolveInfo appInfo = getItem(position);
-        ((ImageView) convertView.findViewById(R.id.icon)).setImageDrawable(appInfo.loadIcon(packageManager));
+		final ResolveInfo appInfo = getItem(position);
+		((ImageView) convertView.findViewById(R.id.icon)).setImageDrawable(appInfo.loadIcon(packageManager));
 
-        ((TextView) convertView.findViewById(R.id.name)).setText(appInfo.loadLabel(packageManager));
+		((TextView) convertView.findViewById(R.id.name)).setText(appInfo.loadLabel(packageManager));
 
-        View.OnClickListener onClick = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent launch_intent = createLauncherIntent();
-                launch_intent.setComponent(new ComponentName(appInfo.activityInfo.packageName, appInfo.activityInfo.name));
-                launch_intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(launch_intent);
-            }
-        };
-        convertView.setOnClickListener(onClick);
-        return convertView;
-    }
+		View.OnClickListener onClick = new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent launch_intent = createLauncherIntent();
+				launch_intent.setComponent(new ComponentName(appInfo.activityInfo.packageName, appInfo.activityInfo.name));
+				launch_intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				context.startActivity(launch_intent);
+			}
+		};
+		convertView.setOnClickListener(onClick);
+		return convertView;
+	}
 
-    @NonNull
-    private static Intent createLauncherIntent() {
-        Intent intent = new Intent();
-        intent.addCategory(CATEGORY_LAUNCHER);
-        intent.setAction(ACTION_MAIN);
-        return intent;
-    }
+	@NonNull
+	private static Intent createLauncherIntent() {
+		Intent intent = new Intent();
+		intent.addCategory(CATEGORY_LAUNCHER);
+		intent.setAction(ACTION_MAIN);
+		return intent;
+	}
 
 }
